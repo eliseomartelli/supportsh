7c45990d8a8287b92ad9dafc247385cefe8fc655
Eliseo Martelli <me@eliseomartelli.it>
2020-04-20 17:44:12 +0200

    🐛 Try to fix a little bug

--- support.sh
+++ support.sh
@@ -3,7 +3,7 @@
 SERVER="support.eliseomartelli.it"
 SUPPORT_USER="support"
 printf "\nStarting support connection to $SERVER\n\n"
-screen -S "support" -d -m bash -c "ssh -N -R 2222:localhost:22 -o CheckHostIP=no $SUPPORT_USER@$SERVER"
+screen -S "support" -d -m bash -c "ssh -N -R 2222:localhost:22 $SUPPORT_USER@$SERVER -o \"CheckHostIP=no\""
 read  -n 1 -p "Press any button to end"
 screen -XS "support" quit
 printf "\nBye!\n\n"
