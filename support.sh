004d11fc76307dcef4027c02ede9c92278e687e6
Eliseo Martelli <me@eliseomartelli.it>
2020-04-21 19:29:05 +0200

    :green_heart: Respect the linter

--- support.sh
+++ support.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-SERVER="support.eliseomartelli.it"
+SERVER="bw.gcloud.eliseomartelli.it"
 SUPPORT_USER="support"
-printf "\nStarting support connection to $SERVER\n\n"
+printf "\nStarting support connection to %s\n\n" $SERVER
 screen -S "support" -d -m bash -c "ssh -N -R 2222:localhost:22 $SUPPORT_USER@$SERVER"
-read  -n 1 -p "Press any button to end"
+read  -n 1 -pr "Press any button to end"
 screen -XS "support" quit
 printf "\nBye!\n\n"
